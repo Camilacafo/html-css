68a3a2a071f939e70b5cc0e4e0a690b0a1f7e442
Camila <166883516+Camilacafo@users.noreply.github.com>
2024-08-08 15:25:59 -0300

    Parâmetros de formulário

---
+++ exercicios/ex025/cadastro.php
@@ -0,0 +1,2 @@
+kjhfliyfçliyfilçyfiçfilyfilyfliycluyciydolfliyflfylifipfi
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus nam nulla corporis, tenetur velit cumque deleniti reprehenderit quos a odit tempora accusamus et tempore necessitatibus ipsa mollitia! Beatae, similique perspiciatis.
