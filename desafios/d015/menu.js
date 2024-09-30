bd5a4b16f113ffed81185faa115fe03a1b17cb28
Camila <166883516+Camilacafo@users.noreply.github.com>
2024-09-30 14:33:07 -0300

    Treinando um pouco de JS

---
+++ desafios/d015/menu.js
@@ -0,0 +1,14 @@
+function clicar1() {
+    window.open('/desafios/d015/testemenu.html', '_blank')
+}
+
+function entrar() {
+    var ent = window.document.getElementById('opcao2')
+    ent.innerText = 'Entrou!'
+    ent.style.background='green'
+}
+
+function mais() {
+    var entrar2 = window.document.querySelector('a#opcao3')
+    entrar2.innerText=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam saepe cumque suscipit pariatur doloremque facere, ut esse cupiditate, dolorum omnis, officiis earum quisquam fugit praesentium. Architecto iusto impedit distinctio explicabo!LoremLorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam saepe cumque suscipit pariatur doloremque facere, ut esse cupiditate, dolorum omnis, officiis earum quisquam fugit praesentium. Architecto iusto impedit distinctio explicabo!Lorem  '
+}
