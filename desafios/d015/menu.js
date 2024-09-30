25458590194152e9ca057ae2634275f99c8c008b
Camila <166883516+Camilacafo@users.noreply.github.com>
2024-09-30 14:57:40 -0300

    att

--- desafios/d015/menu.js
+++ desafios/d015/menu.js
@@ -1,8 +1,10 @@
-function clicar1() {
+
+ 
+    function clicar1() {
     window.open('/desafios/d015/testemenu.html', '_blank')
 }
 
-function entrar() {
+function entrar1() {
     var ent = window.document.getElementById('opcao2')
     ent.innerText = 'Entrou!'
     ent.style.background='green'
@@ -12,3 +14,6 @@ function mais() {
     var entrar2 = window.document.querySelector('a#opcao3')
     entrar2.innerText=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam saepe cumque suscipit pariatur doloremque facere, ut esse cupiditate, dolorum omnis, officiis earum quisquam fugit praesentium. Architecto iusto impedit distinctio explicabo!LoremLorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam saepe cumque suscipit pariatur doloremque facere, ut esse cupiditate, dolorum omnis, officiis earum quisquam fugit praesentium. Architecto iusto impedit distinctio explicabo!Lorem  '
 }
+
+   
+    
